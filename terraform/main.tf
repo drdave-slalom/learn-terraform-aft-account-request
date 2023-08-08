1c207ddfa7f015aeb46221a1b61c682e2d27b5d4
David Ramanauskas <david.ramanauskas@slalom.com>
2023-08-08 09:56:10 -0500

    Create sandbox-aft account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "david129@sbcglobal.net"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "david129@sbcglobal.net"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
